22d404962bd2109428e8bd458a7e77ffdf58a292
Jarch Hu <jarhu@sierrawireless.com>
2019-09-09 17:22:39 +0800

    update check_baseline.sh to support qti prop

--- check_baseline.sh
+++ check_baseline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+check_prop=0
 URL="git://cnshz-er-git01"
 resultfile=$PWD/result.log
 
@@ -16,6 +16,15 @@ $0 <options ...>
     -r <swi repo xml, ex: amss.xml>
     -w <swi main workspace directory>
     -t [target output dir, default outdir if missing.]
+    #If you want to update yocto baseline from SDX55_apps/apps_proc/poky meta-qti-*-prop, please also 
+    check with -p and -l as below:
+    -p [previouse qti commit, the last commit swi merged baseline.]
+    -l [latest qti prop commit, you can get from le-1.0 branch]
+ 
+ Ex:
+    $0 -o LE.UM.3.3.2-02500-SDX55.xml -n LE.UM.3.3.2-03000-SDX55.xml -w /home/jarhu/work/em91/em919x/ -r /home/jarhu/work/em91/em919x/.repo/manifests/sdx55/branches/sdx55r10_em919x/amss.xml   
+    or
+    $0 -o LE.UM.3.3.2-02500-SDX55.xml -n LE.UM.3.3.2-03000-SDX55.xml -w /home/jarhu/work/em91/em919x/ -r /home/jarhu/work/em91/em919x/.repo/manifests/sdx55/branches/sdx55r10_em919x/amss.xml  -p 0b1c6f1af -l fc1233eb1 
 EOF
     exit 1
 }
@@ -53,7 +62,53 @@ check_missing()
     return 1
 }
 
-while getopts "o::n:r:t:w:" arg
+check_patch()
+{
+    patchfile=$1
+    [ ! -f $patchfile ] && echo "$patchfile is not exist" && exit 1
+	#search every diff file in patch file to check if it should be updated in swiwkdir.
+	for diff_file in `cat $patchfile | grep "^diff --git" | awk '{print $4}'`
+	do
+		diff_file=${diff_file##b/}
+		#echo check $diff_file in $patchfile of $meta
+		#echo if it is new file
+		cat $patchfile | grep -A1 "^diff --git" | grep -A1 "$diff_file"  | grep "new file" &>/dev/null
+		if [ $? -eq 0 ];then
+			echo -en "$tmpdir/$meta/$diff_file\n" >> $maindir/newfile.txt
+			echo -en "$tmpdir/$meta/$diff_file is new file,refer to $outdir/$meta/$patchfile\n" >> $maindir/newfile_verbose.txt
+			continue
+		fi
+
+		#echo if it is deleted file
+		cat $patchfile | grep -A1 "^diff --git" | grep -A1 "$diff_file"  | grep "deleted file" &>/dev/null
+		if [ $? -eq 0 ];then
+			echo -en "$tmpdir/$meta/$diff_file\n" >> $maindir/delfile.txt
+			echo -en "$tmpdir/$meta/$diff_file is deleted file,refer to $outdir/$meta/$patchfile\n" >> $maindir/delfile_verbose.txt
+			continue
+		fi
+
+		#is a normal file, should search in swi workspace dir.
+		basediff_file=${diff_file##*/}
+		FIND=
+		for searchdir in `echo $swiwkdir/meta-*`
+		do
+			FINDTMP=`find $searchdir/ -name "$basediff_file"`
+			[ "x$FINDTMP" != "x" ] && FIND="$FINDTMP $FIND"
+		done
+
+		if [ "x$FIND" != "x" ];then
+			echo "-------------------------------------------------------------------------------------------" >>$maindir/updatefile_verbose.txt
+			echo -en "$tmpdir/$meta/$diff_file\n" >>$maindir/updatefile.txt
+			echo -en "$tmpdir/$meta/$diff_file updated,refer to $outdir/$meta/$patchfile\n" >> $maindir/updatefile_verbose.txt
+			echo -en "Should update:\n$FIND\n" >> $maindir/updatefile_verbose.txt
+			echo "============================================================================================" >>$maindir/updatefile_verbose.txt
+		fi
+	done
+}
+
+
+
+while getopts "o:n:r:t:w:p:l:" arg
 do
     case $arg in
     o)
@@ -75,6 +130,15 @@ do
         outdir=$OPTARG
         #echo "outdir: $outdir"
         ;;
+    p)
+        check_prop=1
+        pre_commit=$OPTARG
+        #echo "outdir: $outdir"
+        ;;
+    l)
+        latest_commit=$OPTARG
+        #echo "outdir: $outdir"
+        ;;
     w)
         swiwkdir=$OPTARG
         #echo "swi workspace dir: $swiwkdir"
@@ -89,6 +153,10 @@ done
 
 if [[ -z $oldxml || -z $newxml || -z $repoxml || -z $swiwkdir ]];then
     echo "Miss paramer" && usage 
+fi
+
+if [ $check_prop -eq 1 ];then
+    [[ -z $pre_commit || -z $latest_commit ]]  && echo "Miss parameter" &&usage
 fi
 
 maindir=$PWD/
@@ -108,6 +176,10 @@ check_missing "$oldmetalst" "$newmetalst" "Check old meta list in new xml"
 check_missing "$newmetalst" "$oldmetalst" "Check new meta list in old xml"
 [ $? -ne 0 ] && echo "list is not same" && exit 1
 
+rm -f $maindir/updatefile.txt $maindir/newfile.txt $maindir/delfile.txt
+rm -f $maindir/updatefile_verbose.txt $maindir/newfile_verbose.txt $maindir/delfile_verbose.txt
+
+echo -n "Check yocto patch for qti opensource....."
 for meta in $oldmetalst
 do
     METAPATH=`cat $repoxml  | grep "$meta" | grep -oe "<project name=\S*" | awk -F'"' '{print $2}'`
@@ -144,68 +216,96 @@ do
         exit 1
     fi
 
-
     #search every patch file.
     cd $outdir/$meta
     for patchfile in `find . -name '*.patch'` 
     do
-        #search every diff file in patch file to check if it should be updated in swiwkdir.
-        for diff_file in `cat $patchfile | grep "^diff --git" | awk '{print $4}'`
-        do
-            diff_file=${diff_file##b/}
-            #echo check $diff_file in $patchfile of $meta
-            #echo if it is new file
-            cat $patchfile | grep -A1 "^diff --git" | grep -A1 "$diff_file"  | grep "new file" &>/dev/null
-            if [ $? -eq 0 ];then
-                echo -en "$tmpdir/$meta/$diff_file\n" >> $maindir/newfile.txt
-                echo -en "$tmpdir/$meta/$diff_file is new file,refer to $outdir/$meta/$patchfile\n" >> $resultfile
-                continue
-            fi
-
-            #echo if it is deleted file
-            cat $patchfile | grep -A1 "^diff --git" | grep -A1 "$diff_file"  | grep "deleted file" &>/dev/null
-            if [ $? -eq 0 ];then
-                echo -en "$tmpdir/$meta/$diff_file\n" >> $maindir/delfile.txt
-                echo -en "$tmpdir/$meta/$diff_file is deleted file,refer to $outdir/$meta/$patchfile\n" >> $resultfile
-                continue
-            fi
-
-            #is a normal file, should search in swi workspace dir.
-            basediff_file=${diff_file##*/}
-            FIND=
-            for searchdir in `echo $swiwkdir/meta-*`
-            do
-                FINDTMP=`find $searchdir/ -name "$basediff_file"`
-                [ "x$FINDTMP" != "x" ] && FIND="$FINDTMP $FIND"
-            done
-            
-            if [ "x$FIND" != "x" ];then
-                echo "-------------------------------------------------------------------------------------------" >>$resultfile
-                echo -en "$tmpdir/$meta/$diff_file\n" >>$maindir/updatefile.txt
-                echo -en "$tmpdir/$meta/$diff_file updated,refer to $outdir/$meta/$patchfile\n" >> $resultfile
-                echo -en "Should update:\n$FIND\n" >> $resultfile
-                echo "============================================================================================" >>$resultfile
-            fi
-        done
+        check_patch $patchfile 
     done
-    
     cd $maindir
 done
+echo "[Done]"
+
+#Check patch for qti prop yocto.
+#download git repository.
+if [ $check_prop -eq 1 ];then
+    echo -n "Check yocto patch for qti prop....."
+    cd $maindir
+    prop_patchdir=$maindir/prop_patch
+    mkdir -p $prop_patchdir
+
+    PROP_URL="$URL"/external/qct/sdx55
+    git clone --bare $PROP_URL sdx55.git
+    [ $? -ne 0 ] && "git clone --bare $PROP_URL fail" && exit
+    
+    LOCALGIT=$maindir/sdx55.git
+
+    echo "Clone latest commit"
+    git clone $LOCALGIT sdx55_new
+    [ $? -ne 0 ] && "Clone sdx55_new fail" && exit
+    cd sdx55_new
+    git checkout $latest_commit
+    [ $? -ne 0 ] && "Check out latest_commit:$latest_commit fail" && exit
+    new_prop=$maindir/sdx55_new/SDX55_apps/apps_proc/poky
+    cd -
+    
+    echo "Clone previous commit"
+    git clone $LOCALGIT sdx55_old
+    [ $? -ne 0 ] && "Clone sdx55_old fail" && exit
+    cd sdx55_old
+    git checkout $pre_commit
+    [ $? -ne 0 ] && "Check out pre_commit:$pre_commit fail" && exit
+    old_prop=$maindir/sdx55_old/SDX55_apps/apps_proc/poky
+    cd -
+   
+    #Generate updated and new files.
+    cd $old_prop
+    cp -rf $new_prop/* .
+    git diff $old_prop >$prop_patchdir/prop_updated_files.patch
+    for next in $( git ls-files --others --exclude-standard ) ; do git --no-pager diff --no-index /dev/null $next; done | tee $prop_patchdir/prop_new_files.patch
+    #remove the new and updated files.
+    for next in $( git ls-files --others --exclude-standard ) ; do rm -f $next; done
+    
+    #Generate deleted files, it means the files only in old path are the deleted files.
+    cd $new_prop
+    cp -rf $old_prop/* .
+    for next in $( git ls-files --others --exclude-standard ) ; do git --no-pager diff --no-index $next /dev/null ; done | tee $prop_patchdir/prop_deleted_files.patch
+    #remove deleted and updated files.
+    for next in $( git ls-files --others --exclude-standard ) ; do rm -f $next; done
+    git checkout ./
+    
+    #search every patch file.
+    cd $prop_patchdir
+    for patchfile in `find . -name '*.patch'` 
+    do
+        check_patch $patchfile 
+    done
+    cd $maindir
+    echo "[Done]"
+fi
+
+echo "Updated files:" >$resultfile
+cat $maindir/updatefile_verbose.txt >>$resultfile
+echo -e "\n\n\n" >>$resultfile
+
+echo "New files:" >>$resultfile
+cat $maindir/newfile_verbose.txt >>$resultfile
+echo -e "\n\n\n" >>$resultfile
+
+echo "Deleted files:" >>$resultfile
+cat $maindir/delfile_verbose.txt >>$resultfile
 
 printf -v list_start '%.0s-' {1..120}
-
 echo -en "Files list summary, please get the details from $resultfile\n"
 echo $list_start
 echo -en "\e[1;32mNew file list:\n"
 cat $maindir/newfile.txt
 echo -en "\e[0m"
 
-
 echo $list_start
 echo -en "\e[1;31mDeleted file list:\n"
 cat $maindir/delfile.txt
 echo -en "\e[0m"
-
 
 echo $list_start
 echo -en "\e[1;33mUpdate file list:\n" 
@@ -213,4 +313,4 @@ cat $maindir/updatefile.txt
 echo -en "\e[0m"
 
 rm -f $maindir/updatefile.txt $maindir/newfile.txt $maindir/delfile.txt
-
+rm -f $maindir/updatefile_verbose.txt $maindir/newfile_verbose.txt $maindir/delfile_verbose.txt
