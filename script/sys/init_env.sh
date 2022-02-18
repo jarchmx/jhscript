490c61134021a7e0b16cbc6928c586d0bd960814
Jarch Hu <jarch_hu@askey.com>
2022-02-18 23:02:55 +0800

    added tmux.service
    Signed-off-by: Jarch Hu <jarch_hu@askey.com>

--- script/sys/init_env.sh
+++ script/sys/init_env.sh
@@ -4,73 +4,50 @@
 
 CURRENT_PROG_DIR=$(dirname `realpath $0`)
 MAIN_DIR=$(realpath $CURRENT_PROG_DIR/../../)
-SCRIPT_DIR="$MAIN_DIR/script/"
-ENV_DIR="$MAIN_DIR/env/"
-CFG_DIR="$MAIN_DIR/cfg/"
-
-SYS_SCRIPTS="\
-	sys/tmux_init \
-	sys/tmux-session \
-	vpn/forti.sh \
-	security/pil-splitter.py \
-	security/parsecerts.sh \
-	module/ramdump-parser.sh \
-"
+#SCRIPT_DIR="$MAIN_DIR/script/"
 
 # src_file, target_dir
-ENV_CONF="\
-   .netrc,$HOME \
-   .gitconfig,$HOME \
-   .vimrc,$HOME \
-   .bashrc,$HOME \
-   ssh/config,$HOME/.ssh \
+#ENV_CONF="\
+LK_FILES="\
+    env/.netrc,$HOME \
+    env/.gitconfig,$HOME \
+    env/.vimrc,$HOME \
+    env/.bashrc,$HOME \
+    env/ssh/config,$HOME/.ssh \
+    cfg/eclipse_template,$HOME/workspace/ \
+    script/sys/tmux-session,$HOME/bin/ \
+    script/vpn/forti.sh,$HOME/bin/ \
+    script/security/pil-splitter.py,$HOME/bin/ \
+    script/security/parsecerts.sh,$HOME/bin/ \
+    script/module/ramdump-parser.sh,$HOME/bin/ \
+    script/sys/tmux-session,/usr/bin/,sudo \
+    env/service/tmux.service,/lib/systemd/system/,sudo \
 "
 
-CFG_CONF="\
-   eclipse_template,$HOME/workspace/ \
+RUN_COMMANDS=" \
+    sudo systemctl enable tmux, \
 "
 
-link_sys_scripts()
+link_files()
 {
-    [ ! -d $HOME/bin ] && mkdir $HOME/bin
-    
-    for script in $SYS_SCRIPTS
+    for lk in $LK_FILES
     do
-        [ -f $HOME/bin/$script ] && rm -f $HOME/bin/$script
-        ln -sf $SCRIPT_DIR/$script $HOME/bin/
-    done
-}
-
-link_env()
-{
-    for env in $ENV_CONF
-    do
-        set `echo $env | awk -F',' '{printf("%s %s\n",$1,$2)}'`
-        src_file=$ENV_DIR/$1
+        set `echo $lk | awk -F',' '{printf("%s %s %s\n",$1,$2,$3)}'`
+        src_file=$MAIN_DIR/$1
         target_dir=$2
+        sys_sudo=$3
         target_file=$target_dir/$(basename $1)
-        [ -L $target_file ] && rm -f $target_file
-        [ ! -d $target_dir ] && mkdir -p $target_dir
-        ln -sf $src_file $target_file
+        [ -L $target_file ] && $sys_sudo rm -f $target_file
+        [ ! -d $target_dir ] && $sys_sudo mkdir -p $target_dir
+        $sys_sudo ln -sf $src_file $target_file
     done
 }
 
-link_cfg()
+run_commands()
 {
-    for cfg in $CFG_CONF
-    do
-        set `echo $cfg | awk -F',' '{printf("%s %s\n",$1,$2)}'`
-        src_file=$CFG_DIR/$1
-        target_dir=$2
-        target_file=$target_dir/$(basename $1)
-        [ -L $target_file ] && rm -f $target_file
-        [ ! -d $target_dir ] && mkdir -p $target_dir
-        ln -sf $src_file $target_file
-    done
+    echo $RUN_COMMANDS | awk 'BEGIN{FS=",";count=1} {for(i=1;i<=NF;i++) { system($i)}}'
 }
 
-link_sys_scripts
+link_files
 
-link_env
-
-link_cfg
+run_commands
