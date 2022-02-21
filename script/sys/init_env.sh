bf0d98f63008c2eab9f8044cce1fad88eb556a73
Jarch Hu <jarch_hu@askey.com>
2022-02-21 12:40:45 +0800

    update init_env.sh

--- script/sys/init_env.sh
+++ script/sys/init_env.sh
@@ -1,53 +1,39 @@
 #!/bin/sh
-# Save and restore the state of tmux sessions and windows.
-# TODO: persist and restore the state & position of panes.
 
 CURRENT_PROG_DIR=$(dirname `realpath $0`)
 MAIN_DIR=$(realpath $CURRENT_PROG_DIR/../../)
-#SCRIPT_DIR="$MAIN_DIR/script/"
 
-# src_file, target_dir
-#ENV_CONF="\
-LK_FILES="\
-    env/.netrc,$HOME \
-    env/.gitconfig,$HOME \
-    env/.vimrc,$HOME \
-    env/.bashrc,$HOME \
-    env/ssh/config,$HOME/.ssh \
-    cfg/eclipse_template,$HOME/workspace/ \
-    script/sys/tmux-session,$HOME/bin/ \
-    script/vpn/forti.sh,$HOME/bin/ \
-    script/security/pil-splitter.py,$HOME/bin/ \
-    script/security/parsecerts.sh,$HOME/bin/ \
-    script/module/ramdump-parser.sh,$HOME/bin/ \
-    script/sys/tmux-session,/usr/bin/,sudo \
-    env/service/tmux.service,/lib/systemd/system/,sudo \
-"
-
-RUN_COMMANDS=" \
+COMMANDS_LIST="\
+    ln -sf $MAIN_DIR/env/.netrc $HOME, \
+    ln -sf $MAIN_DIR/env/.gitconfig $HOME, \
+    ln -sf $MAIN_DIR/env/.vimrc $HOME, \
+    ln -sf $MAIN_DIR/env/.bashrc $HOME, \
+    mkdir -p $HOME/.ssh, \
+    mkdir -p $HOME/bin/, \
+    ln -sf $MAIN_DIR/env/ssh/config $HOME/.ssh, \
+    ln -sf $MAIN_DIR/cfg/eclipse_template $HOME/workspace/, \
+    ln -sf $MAIN_DIR/script/sys/tmux-session $HOME/bin/, \
+    ln -sf $MAIN_DIR/script/vpn/forti.sh $HOME/bin/, \
+    ln -sf $MAIN_DIR/script/security/pil-splitter.py $HOME/bin/, \
+    ln -sf $MAIN_DIR/script/security/parsecerts.sh $HOME/bin/, \
+    ln -sf $MAIN_DIR/script/module/ramdump-parser.sh $HOME/bin/, \
+    sudo rm -f /usr/bin/tmux-session, \
+    sudo cp $MAIN_DIR/script/sys/tmux-session /usr/bin/, \
+    sudo rm -f /lib/systemd/system/tmux.service, \
+    sudo cp $MAIN_DIR/env/service/tmux.service /lib/systemd/system/, \
     sudo systemctl enable tmux, \
 "
 
-link_files()
-{
-    for lk in $LK_FILES
-    do
-        set `echo $lk | awk -F',' '{printf("%s %s %s\n",$1,$2,$3)}'`
-        src_file=$MAIN_DIR/$1
-        target_dir=$2
-        sys_sudo=$3
-        target_file=$target_dir/$(basename $1)
-        [ -L $target_file ] && $sys_sudo rm -f $target_file
-        [ ! -d $target_dir ] && $sys_sudo mkdir -p $target_dir
-        $sys_sudo ln -sf $src_file $target_file
-    done
-}
-
 run_commands()
 {
-    echo $RUN_COMMANDS | awk 'BEGIN{FS=",";count=1} {for(i=1;i<=NF;i++) { system($i)}}'
+    echo $COMMANDS_LIST | awk 'BEGIN{FS=",";count=1} \
+    { \
+        for(i=1;i<=NF;i++)\
+        { \
+            rc=system($i) \
+        }\
+    }'
 }
 
-link_files
 
 run_commands
