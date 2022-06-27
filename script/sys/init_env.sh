01f10f18ec4597e8ae94fc38af673b729b94a280
Jarch Hu <jarch_hu@askey.com>
2022-06-27 10:00:27 +0800

    update tmux.service for all user
    Signed-off-by: Jarch Hu <jarch_hu@askey.com>

--- script/sys/init_env.sh
+++ script/sys/init_env.sh
@@ -20,9 +20,9 @@ COMMANDS_LIST="\
     ln -sf $MAIN_DIR/script/module/ramdump-parser.sh $HOME/bin/, \
     sudo rm -f /usr/bin/tmux-session, \
     sudo cp $MAIN_DIR/script/sys/tmux-session /usr/bin/, \
-    sudo rm -f /lib/systemd/system/tmux.service, \
-    sudo cp $MAIN_DIR/env/service/tmux.service /lib/systemd/system/, \
-    sudo systemctl enable tmux, \
+    sudo rm -f /lib/systemd/system/tmux@.service, \
+    sudo cp $MAIN_DIR/env/service/tmux@.service /lib/systemd/system/, \
+    sudo systemctl enable tmux@.service, \
 "
 
 run_commands()
